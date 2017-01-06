0902647667bc0f75e008b80bb4ef56b6735b3b2f
Lord Chika Onyebueke <onye108@icloud.com>
2017-01-05 20:06:24 -0800

    kinda functional

---
+++ objectData.js
@@ -0,0 +1,26 @@
+var projectsArray =[
+  {
+    title: 'Salmon Cookies',
+    imageUrl:  'assets/salmon.png',
+    projectPage: 'https://onye108.github.io/cookie-stand/',
+    dateCreated:'2016/12/15'
+  },
+  {
+    title: 'Bus Mall',
+    imageUrl:'images/busmallimg.png',
+    projectPage:'https://onye108.github.io/bus-mall/',
+    dateCreated: '2016/12/18'
+  },
+  {
+    title: 'About Me (app)',
+    imageUrl: '',
+    projectPage:'https://onye108.github.io/about_me/',
+    dateCreated:'2016/12/22'
+  },
+  {
+    title: 'Battleships!',
+    imageUrl: 'assets/DestroyerDestroying.jpg',
+    projectPage:'https://onye108.github.io/battleship/',
+    dateCreated:'2016/12/22'
+  }
+];
