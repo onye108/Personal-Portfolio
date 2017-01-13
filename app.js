e15b70e7f33300cca527669a3ada659507cd3a42
Lord Chika Onyebueke <onye108@icloud.com>
2017-01-12 22:17:55 -0800

    redo jq portfolio slide

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
-var portfolio = [];
 
-function Project(options){
-  for (var key in options) {
-    this[key] = options[key];
+$(document).ready(function(){
+  $('#myNavbar a').on('click', function(){
+    $('#myNavbar  li.current').removeClass('current');
+    $(this).parent().addClass('current');
+  })
   }
+  $('h1#heading').text($(this).text());
 
-}
-
-Project.prototype.toHtml = function(){
-  var source = $('#project-template').html();
-  var templateRender = Handlebars.compile(source);
-  this.daysAgo = parseInt((new Date() - new Date(this.dateCreated))/60/60/24/1000);
-  this.publishStatus = this.dateCreated ? `published ${this.daysAgo} days ago`:
-  '(draft)';
-  return templateRender(this);
-};
-projectsArray.sort(function(a, b) {
-  return (new Date(b.dateCreated)) - (new Date(a.dateCreated));
-});
-projectsArray.forEach(function(element) {
-  portfolio.push(new Project(element));
-});
-portfolio.forEach(function(a) {
-$('.carousel-inner > .item').append(a.toHtml());
 });
--- objectData.js
+++ objectData.js
@@ -1,25 +1,25 @@
 projectsArray =[
-    {
-    title: 'Salmon Cookies',
-    imageUrl:  src="assets/salmon.png" height="100px" width="300px" ,
+  {
+    projectName:'Salmon Cookies',
+    imageUrl: 'assets/salmon.png',
     projectPage: 'https://onye108.github.io/cookie-stand/',
     dateCreated:'2016/12/15'
   },
   {
-    title: 'Bus Mall',
-    imageUrl:'images/busmallimg.png',
+    projectName: 'Bus Mall',
+    imageUrl: 'images/busmallimg.png',
     projectPage:'https://onye108.github.io/bus-mall/',
     dateCreated: '2016/12/18'
   },
   {
-    title: 'About Me (app)',
-    imageUrl: '',
+    projectName: 'About Me (app)',
+    imageUrl: 'assets/download.jpeg',
     projectPage:'https://onye108.github.io/about_me/',
     dateCreated:'2016/12/22'
   },
   {
-    title: 'Battleships!',
-    imageUrl: "assets/DestroyerDestroying.jpg" height="100px" width="300px",
+    projectName: 'Battleships!',
+    imageUrl: 'assets/DestroyerDestroying.jpg',
     projectPage:'https://onye108.github.io/battleship/',
     dateCreated:'2016/12/22'
   }
